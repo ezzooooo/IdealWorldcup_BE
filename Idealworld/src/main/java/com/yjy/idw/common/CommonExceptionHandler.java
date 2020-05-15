cc47a58476dd8a1590e9eb5d1a0c078e95cf9f44
LeeJuWon <dkreka11@gmail.com>
2020-05-15 21:42:07 +0900

    fileupload test

---
+++ Idealworld/src/main/java/com/yjy/idw/common/CommonExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.yjy.idw.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice("com.leeyunbo.view")
+public class CommonExceptionHandler {
+	
+	@ExceptionHandler(ArithmeticException.class)
+	public ModelAndView handleArithmeticException(Exception e) {
+		ModelAndView mav = new ModelAndView(); 
+		mav.addObject("exception",e); 
+		mav.setViewName("/common/arithmeticError.jsp");
+		return mav; 
+	}
+	
+	@ExceptionHandler(NullPointerException.class)
+	public ModelAndView handlerNullPointerException(Exception e) {
+		ModelAndView mav = new ModelAndView(); 
+		mav.addObject("exception",e); 
+		mav.setViewName("/common/nullPointerError.jsp");
+		return mav;
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handlerException(Exception e) {
+		ModelAndView mav = new ModelAndView(); 
+		mav.addObject("exception", e); 
+		mav.setViewName("/common/error.jsp");
+		return mav;
+	}
+	
+	
+}
---
+++ Idealworld/src/main/java/com/yjy/idw/image/ImageService.java
@@ -0,0 +1,5 @@
+package com.yjy.idw.image;
+
+public interface ImageService {
+	void insertImage(ImageVO vo);
+}
