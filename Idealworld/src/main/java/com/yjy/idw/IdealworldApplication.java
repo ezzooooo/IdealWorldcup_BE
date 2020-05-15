5362e703bd62868d9d719a01b9d6e5d88838dbe2
LeeJuWon <dkreka11@gmail.com>
2020-05-15 19:48:13 +0900

    New Project

---
+++ Idealworld/src/main/java/com/yjy/idw/IdealworldApplication.java
@@ -0,0 +1,13 @@
+package com.yjy.idw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdealworldApplication.class, args);
+	}
+
+}
---
+++ Idealworld/src/test/java/com/yjy/idw/IdealworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjy.idw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
