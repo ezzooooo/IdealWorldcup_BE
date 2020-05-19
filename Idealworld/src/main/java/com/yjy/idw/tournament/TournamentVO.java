5606c0dd2e7054b25678ed0d3446c1a9ca059c6f
leeyunbo <servers1@naver.com>
2020-05-19 23:08:53 +0900

    [개발] TournamentVO 개발

--- Idealworld/src/main/java/com/yjy/idw/tournament/TournamentVO.java
+++ Idealworld/src/main/java/com/yjy/idw/tournament/TournamentVO.java
@@ -1,5 +1,61 @@
 package com.yjy.idw.tournament;
 
 public class TournamentVO {
-
+	int id; 
+	int user_id; 
+	String title; 
+	String intro; 
+	String category; 
+	String isPrivate;
+	int like_cnt; 
+	int play_cnt;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getIntro() {
+		return intro;
+	}
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getIsPrivate() {
+		return isPrivate;
+	}
+	public void setIsPrivate(String isPrivate) {
+		this.isPrivate = isPrivate;
+	}
+	public int getLike_cnt() {
+		return like_cnt;
+	}
+	public void setLike_cnt(int like_cnt) {
+		this.like_cnt = like_cnt;
+	}
+	public int getPlay_cnt() {
+		return play_cnt;
+	}
+	public void setPlay_cnt(int play_cnt) {
+		this.play_cnt = play_cnt;
+	} 
 }
