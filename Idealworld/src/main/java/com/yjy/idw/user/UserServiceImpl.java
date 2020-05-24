1e7c967c10cb828a6db5e1cfb5ccf3bff6411bd6
leeyunbo <servers1@naver.com>
2020-05-24 20:44:10 +0900

    [수정] ImageServiceImpl 수정

--- Idealworld/src/main/java/com/yjy/idw/user/UserServiceImpl.java
+++ Idealworld/src/main/java/com/yjy/idw/user/UserServiceImpl.java
@@ -1,35 +1,74 @@
 package com.yjy.idw.user;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.stereotype.Service;
 
 @Service("userService")
-public class UserServiceImpl implements UserService {
-	@Autowired
-	UserDAO userDAO;
+public class UserServiceImpl implements UserService {	
+	static private final String resource = "config/mybatis-config.xml";
+	static private InputStream inputStream = null; 
+	static private SqlSessionFactory sqlSessionFactory = null;
+	
+	static {
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	public List<UserVO> getUserList() {
-		// TODO Auto-generated method stub
-		return userDAO.getUserList();
+		List<UserVO> userList;
+		
+		try(SqlSession session = sqlSessionFactory.openSession()) {
+			UserDAO image = session.getMapper(UserDAO.class);
+			userList = image.getUserList();
+			session.commit();
+			session.close();
+		}
+		
+		return userList;
 	}
 
 	@Override
 	public void insertUser(UserVO vo) {
-		userDAO.insertUser(vo);
+		try(SqlSession session = sqlSessionFactory.openSession()) {
+			UserDAO image = session.getMapper(UserDAO.class);
+			image.insertUser(vo);
+			session.commit();
+			session.close();
+		}
 	}
 
 	@Override
 	public UserVO getUser(int id) {
-		// TODO Auto-generated method stub
-		return userDAO.getUser(id);
+		UserVO user;
+		try(SqlSession session = sqlSessionFactory.openSession()) {
+			UserDAO image = session.getMapper(UserDAO.class);
+			user = image.getUser(id);
+			session.commit();
+			session.close();
+		}
+
+		return user;
 	}
 
 	@Override
 	public void deleteUser(int id) {
-		userDAO.deleteUser(id);
-		
+		try(SqlSession session = sqlSessionFactory.openSession()) {
+			UserDAO image = session.getMapper(UserDAO.class);
+			image.deleteUser(id);
+			session.commit();
+			session.close();
+		}
 	}
 }
