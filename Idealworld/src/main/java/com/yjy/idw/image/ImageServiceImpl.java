15bc08431865f00d66663d4cd51ea3376daccf0a
LeeJuWon <dkreka11@gmail.com>
2020-05-24 17:01:50 +0900

    Mybatis를 이용한 DB삽입 성공(Image)

--- Idealworld/src/main/java/com/yjy/idw/image/ImageServiceImpl.java
+++ Idealworld/src/main/java/com/yjy/idw/image/ImageServiceImpl.java
@@ -1,25 +1,41 @@
 package com.yjy.idw.image;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.io.InputStream;
 
-import com.yjy.idw.mybatis.MyBatisConnectionFactory;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.stereotype.Service;
 
 
 @Service("ImageService")
 public class ImageServiceImpl implements ImageService {
 	
-	@Autowired
-	SqlSession sqlSession = MyBatisConnectionFactory.getSqlSessionFactory().openSession();
-	ImageDAO image = sqlSession.getMapper(ImageDAO.class);
-	
 	@Override
 	public int insertImage(ImageVO vo) {
 		System.out.println("여기1");
-		int id = image.insertImage(vo); 
+		
+		String resource = "config/mybatis-config.xml";
+		InputStream inputStream = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+		try(SqlSession session = sqlSessionFactory.openSession()) {
+			
+			ImageDAO image = session.getMapper(ImageDAO.class);
+			image.insertImage(vo);
+			session.commit();
+			session.close();
+		}
+		
 		System.out.println("여기2");
 		
-		return id;
+		return 1;
 	}
 }
