55e4ce7ae3d92487c72a2c1838f72c0686cb7f0a
LeeJuWon <dkreka11@gmail.com>
2020-05-25 13:51:21 +0900

    ImageService 부분 SessionTemplate 사용

--- Idealworld/src/main/java/com/yjy/idw/image/ImageServiceImpl.java
+++ Idealworld/src/main/java/com/yjy/idw/image/ImageServiceImpl.java
@@ -1,37 +1,18 @@
 package com.yjy.idw.image;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service("ImageService")
 public class ImageServiceImpl implements ImageService {
+	
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	
 	@Override
 	public int insertImage(ImageVO vo) {	
-		int result;
-		
-		String resource = "config/mybatis-config.xml";
-		InputStream inputStream = null;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		
-		try(SqlSession session = sqlSessionFactory.openSession()) {
-			ImageDAO image = session.getMapper(ImageDAO.class);
-			result = image.insertImage(vo);
-			session.commit();
-			session.close();
-		}
-		
-		return result;
+		return sqlSessionTemplate.insert("insertImage");
 	}
 }
