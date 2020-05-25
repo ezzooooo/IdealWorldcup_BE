1f5217f6a231a9699c014ffce7abd045dcdc98ee
leeyunbo <servers1@naver.com>
2020-05-25 22:05:45 +0900

    [개발] ResultVO 개발

--- Idealworld/src/main/java/com/yjy/idw/result/ResultVO.java
+++ Idealworld/src/main/java/com/yjy/idw/result/ResultVO.java
@@ -6,5 +6,42 @@ public class ResultVO {
 	int second_image_id; 
 	int first_win_cnt; 
 	int second_win_cnt;
-	String winner; 
+	String winner;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getFirst_image_id() {
+		return first_image_id;
+	}
+	public void setFirst_image_id(int first_image_id) {
+		this.first_image_id = first_image_id;
+	}
+	public int getSecond_image_id() {
+		return second_image_id;
+	}
+	public void setSecond_image_id(int second_image_id) {
+		this.second_image_id = second_image_id;
+	}
+	public int getFirst_win_cnt() {
+		return first_win_cnt;
+	}
+	public void setFirst_win_cnt(int first_win_cnt) {
+		this.first_win_cnt = first_win_cnt;
+	}
+	public int getSecond_win_cnt() {
+		return second_win_cnt;
+	}
+	public void setSecond_win_cnt(int second_win_cnt) {
+		this.second_win_cnt = second_win_cnt;
+	}
+	public String getWinner() {
+		return winner;
+	}
+	public void setWinner(String winner) {
+		this.winner = winner;
+	} 
 }
